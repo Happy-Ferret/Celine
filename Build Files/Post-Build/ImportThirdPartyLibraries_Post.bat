0291a493b4af928ce69071c98eefef90bf42ee59
NPatch <npatsiouras@gmail.com>
2015-01-15 22:13:14 +0200

    Property Sheets,post-build batch file

---
+++ Build Files/Post-Build/ImportThirdPartyLibraries_Post.bat
@@ -0,0 +1,21 @@
+goto comment
+/////////////////////////////////////////////////////////////////////////////
+// Name:        	ImportThirdPartyLibraries_Post.bat
+// Author:      	Nikolaos Patsiouras
+// Email:			npatsiouras@gmail.com
+// Created:     	2015, 1, 15, 8:58 PM
+// Modified by:		Nikos
+// Last Modified:	2015, 1, 15, 8:58 PM
+// Copyright:   	(c) 2015 Nikolaos Patsiouras
+// Licence:     	MIT License
+/////////////////////////////////////////////////////////////////////////////
+:comment
+if %3 == "Win32" (
+	copy %1Dependencies\freeglut\bin\freeglut.dll %1%2\ /Y
+	copy %1Dependencies\glew\bin\glew32.dll %1%2\ /Y
+) else (
+	if %3 == "x64" (
+		copy %1Dependencies\freeglut\bin\x64\freeglut.dll %1%3\%2\ /Y
+		copy %1Dependencies\glew\bin\x64\glew32.dll %1%3\%2\ /Y
+	)
+)
